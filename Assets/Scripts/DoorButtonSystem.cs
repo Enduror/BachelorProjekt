1ab9a1b440a1091c3241c1d22738715f9cb01662
Enduror <teise-rudolf@hotmail.de>
2018-11-07 23:25:26 +0100

    New Prefabs#
    asdasd

---
+++ Assets/Scripts/DoorButtonSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButtonSystem : MonoBehaviour {
+
+    public GameObject door;
+    public TriggerPlate[] steppingStones;
+      
+    void Update()
+    {
+        if (IsAllPressed())
+        {
+            Destroy(door);
+            Destroy(this);
+        }
+    }
+
+    private bool IsAllPressed()
+    {
+        for (int i = 0; i <steppingStones.Length; i++)
+        {
+            if (steppingStones[i].isPressed == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- Assets/Scripts/TriggerPlate.cs
+++ Assets/Scripts/TriggerPlate.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class TriggerPlate : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    public Color pressedColor;
+    public bool isPressed;    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().color = pressedColor;
+            isPressed = true;
+        }
+    }
 }
